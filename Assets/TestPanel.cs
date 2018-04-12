a9c2be05873d0223ff58c7c3db492c8602ee2025
Ron3 <luodm03@qq.com>
2018-04-12 16:22:18 +0800

    -change: 提交了少提的代码.

---
+++ Assets/TestPanel.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestPanel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.Init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	///
+	/// 
+	///
+	public void Init()
+	{
+		GameObject button = this.gameObject.BP_Find("Button");
+		Button buttonComponent = button.GetComponent<Button>();
+		buttonComponent.onClick.AddListener(delegate() {
+            this.OnBtnClick(button);
+        });
+
+		// 注册任务
+		NotificationCenter.DefaultCenter.AddObserver(this, "Finish");
+	}
+
+	///
+	///
+	///
+	public void OnBtnClick(GameObject sender)
+	{
+		Debug.Log("OnBtnClick ===> ");
+		Hashtable data = new Hashtable();
+		data.Add(1, 1);
+		data.Add("2", 2);
+
+		NotificationCenter.DefaultCenter.PostNotification(this, "Finish", data);
+	}
+
+	
+	///
+	/// Summary:
+	/// 这里一点要注意.因为是在遍历那边的数据结构.如果直接又调用删除.直接会导致崩溃
+	public IEnumerator Finish(NotificationCenter.Notification notifyData)
+	{
+		Debug.Log("Finish func call");
+		IDictionaryEnumerator myEnumerator = notifyData.data.GetEnumerator();  
+		while (myEnumerator.MoveNext())
+		{
+			Debug.Log("type ===>" + myEnumerator.Key.GetType());
+			Debug.Log(myEnumerator.Key + " | " + notifyData.data[myEnumerator.Key]);
+		}
+
+		// yield return new WaitForSeconds(0.01f);
+		// NotificationCenter.DefaultCenter.RemoveObserver(this, "Finish");
+		return null;
+	}
+}
